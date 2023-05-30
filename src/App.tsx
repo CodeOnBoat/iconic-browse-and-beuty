986109cf13b7ebfb25466cdf52bc26da22bd1f2b
1998KB <klajdibeqiraj88@gmail.com>
2023-05-30 16:08:50 +0200

    header-landing

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <div className="content-container">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/prices" element={<Prices />} />
           <Route path="/aboutme" element={<AboutMe />} />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,124 +11,93 @@ export const Header = () => {
   };
 
   return (
-    <div className="header-block">
-      <div className="header-container">
-        <div className="header-logo-container">
-          <img src={Logo} alt="" className="logo" />
-        </div>
-        <div className="header-hamburger-menu">
-          <input type="checkbox" id="active" ref={activeRef} />
-          <label htmlFor="active" className="menu-btn">
-            <span></span>
-          </label>
-          <label htmlFor="active" className="close"></label>
-          <div className="wrapper">
-            <ul className="link-burger-container">
-              <li>
-                <Link
-                  className="link-burger"
-                  to={"/"}
-                  onClick={() => toggleActive(false)}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="link-burger"
-                  to={"/"}
-                  onClick={() => toggleActive(false)}
-                >
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="link-burger"
-                  to={"/prices"}
-                  onClick={() => toggleActive(false)}
-                >
-                  Prices
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="link-burger"
-                  to={"/contacts"}
-                  onClick={() => toggleActive(false)}
-                >
-                  Contacts
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="link-burger"
-                  to={"/aboutme"}
-                  onClick={() => toggleActive(false)}
-                >
-                  About me
-                </Link>
-              </li>
-            </ul>
-            <div className="btn-burger-header-container"></div>
+    <>
+      {" "}
+      <div className="header-block">
+        <div className="header-container">
+          <div className="header-logo-container">
+            <img src={Logo} alt="" className="logo" />
           </div>
-        </div>
-        <div className="header-links-container">
-          <div className="link">
-            Features{" "}
-            <svg
-              fill="currentColor"
-              height="8"
-              viewBox="0 0 13 8"
-              width="13"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m11.7305.59279c.3626.362629.3885.93447.0777 1.32699l-.0777.08722-4.99999 4.99999c-.36263.36263-.93446.38853-1.32697.0777l-.08725-.0777-4.999959-4.99997c-.3905249-.39052-.3905242-1.023685 0-1.414209.362629-.36263.934469-.388553 1.326989-.077728l.08722.077728 4.29292 4.292139 4.29284-4.29216c.3626-.36263.9345-.388532 1.327-.077707z"></path>
-            </svg>
+          <div className="header-hamburger-menu">
+            <input type="checkbox" id="active" ref={activeRef} />
+            <label htmlFor="active" className="menu-btn">
+              <span></span>
+            </label>
+            <label htmlFor="active" className="close"></label>
+            <div className="wrapper">
+              <ul className="link-burger-container">
+                <li>
+                  <Link
+                    className="link-burger"
+                    to={"/"}
+                    onClick={() => toggleActive(false)}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-burger"
+                    to={"/"}
+                    onClick={() => toggleActive(false)}
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-burger"
+                    to={"/prices"}
+                    onClick={() => toggleActive(false)}
+                  >
+                    Prices
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-burger"
+                    to={"/contacts"}
+                    onClick={() => toggleActive(false)}
+                  >
+                    Contacts
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-burger"
+                    to={"/aboutme"}
+                    onClick={() => toggleActive(false)}
+                  >
+                    About me
+                  </Link>
+                </li>
+              </ul>
+              <div className="btn-burger-header-container"></div>
+            </div>
           </div>
-          <div className="link">
-            Solutions
-            <svg
-              fill="currentColor"
-              height="8"
-              viewBox="0 0 13 8"
-              width="13"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m11.7305.59279c.3626.362629.3885.93447.0777 1.32699l-.0777.08722-4.99999 4.99999c-.36263.36263-.93446.38853-1.32697.0777l-.08725-.0777-4.999959-4.99997c-.3905249-.39052-.3905242-1.023685 0-1.414209.362629-.36263.934469-.388553 1.326989-.077728l.08722.077728 4.29292 4.292139 4.29284-4.29216c.3626-.36263.9345-.388532 1.327-.077707z"></path>
-            </svg>
+          <div className="links-line-container">
+            <div className="header-line-gradient"></div>
+            <div className="header-links-container">
+              <Link className="link" to={"/home"}>
+                Home
+              </Link>
+              <Link className="link" to={"/home"}>
+                Services
+              </Link>
+              <Link className="link" to={"/prices"}>
+                Prices
+              </Link>
+              <Link className="link" to={"/contacts"}>
+                Contacts
+              </Link>
+              <Link className="link" to={"/aboutme"}>
+                About me
+              </Link>
+            </div>
+            <div className="header-line-gradient"></div>
           </div>
-          <div className="link">
-            Plans
-            <svg
-              fill="currentColor"
-              height="8"
-              viewBox="0 0 13 8"
-              width="13"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m11.7305.59279c.3626.362629.3885.93447.0777 1.32699l-.0777.08722-4.99999 4.99999c-.36263.36263-.93446.38853-1.32697.0777l-.08725-.0777-4.999959-4.99997c-.3905249-.39052-.3905242-1.023685 0-1.414209.362629-.36263.934469-.388553 1.326989-.077728l.08722.077728 4.29292 4.292139 4.29284-4.29216c.3626-.36263.9345-.388532 1.327-.077707z"></path>
-            </svg>
-          </div>
-          <div className="link">Pricing</div>
-          <div className="link">
-            Resources
-            <svg
-              fill="currentColor"
-              height="8"
-              viewBox="0 0 13 8"
-              width="13"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="m11.7305.59279c.3626.362629.3885.93447.0777 1.32699l-.0777.08722-4.99999 4.99999c-.36263.36263-.93446.38853-1.32697.0777l-.08725-.0777-4.999959-4.99997c-.3905249-.39052-.3905242-1.023685 0-1.414209.362629-.36263.934469-.388553 1.326989-.077728l.08722.077728 4.29292 4.292139 4.29284-4.29216c.3626-.36263.9345-.388532 1.327-.077707z"></path>
-            </svg>
-          </div>
-        </div>
-        <div className="btn-header-container">
-          <button className="btn-standard-login">Log in </button>
-          <button className="btn-standard-signin">Create project</button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
